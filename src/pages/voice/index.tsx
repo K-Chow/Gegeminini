c952a5a8d8816e64a13e44dc6397d8f899059dc5
K.Chow <dogegg.chou@gmail.com>
2026-05-13 00:51:38 +0800

    feat: integrate button invoke

--- src/pages/voice/index.tsx
+++ src/pages/voice/index.tsx
@@ -1,11 +1,23 @@
 import { invoke } from '@tauri-apps/api/core'
+import { useState } from 'react'
 
 const Voice = () => {
+  const [isRecording, setIsRecording] = useState(false)
+  const handleStartRecording = async () => {
+    invoke('start_recording')
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.error('Failed to start recording:', err)
+      })
+  }
+
   return (
     <div className="max-h-full flex flex-col h-screen max-w-full">
       <div className="p-4">
-        <button onClick={() => invoke('trigger_recording')}>
-          Start voice recording
+        <button onClick={handleStartRecording}>
+          {isRecording ? 'Stop voice recording' : 'Start voice recording'}
         </button>
       </div>
     </div>
