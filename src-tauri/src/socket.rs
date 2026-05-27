ed88baaf5ff3cfa7c9a4387b4abd52ab5e77edb1
K.Chow <dogegg.chou@gmail.com>
2026-05-28 02:17:42 +0800

    fix: fix worng logic

--- src-tauri/src/socket.rs
+++ src-tauri/src/socket.rs
@@ -14,6 +14,7 @@ pub async fn connect_gemini(
     state: tauri::State<'_, AppState>,
     app_handle: tauri::AppHandle,
 ) -> Result<String, String> {
+    let model = get_model(&state);
     let mut tx_guard = state.gemini_tx.lock().await;
 
     // 1. 防御性检查：确保不重复建立物理连接
@@ -28,15 +29,6 @@ pub async fn connect_gemini(
     // new_rx 由下面的后台网络协程持有，用来接收数据并吐给网络
     let (new_tx, mut new_rx) = tokio::sync::mpsc::unbounded_channel::<GeminiCommand>();
 
-    let setup_msg = serde_json::json!({
-        "setup": {
-            "model": get_model(&state),
-            "generationConfig": {
-                "responseModalities": ["AUDIO"], // 告诉它你需要它用语音回答你
-            }
-        }
-    });
-
     // 3. 产生一个纯粹的常驻后台异步任务，负责死守网络链路
     tokio::spawn(async move {
         println!("⚡ [网络任务] 开始向 Gemini API 终点发起物理握手 (例如 WebSocket)...");
@@ -44,6 +36,20 @@ pub async fn connect_gemini(
         let (mut ws_stream, _) = tokio_tungstenite::connect_async(GEMINI_STREAM_URL)
             .await
             .unwrap();
+
+        let setup_msg = serde_json::json!({
+            "setup": {
+                "model": model,
+                "generationConfig": {
+                    "responseModalities": ["AUDIO"], // 告诉它你需要它用语音回答你
+                }
+            }
+        });
+
+        ws_stream
+            .send(Message::Text(setup_msg.to_string().into()))
+            .await
+            .ok();
 
         println!("✅ [网络任务] 物理连接建立成功！网络协程进入常驻挂起状态，等待发信指令...");
 
@@ -53,10 +59,7 @@ pub async fn connect_gemini(
             match command {
                 GeminiCommand::SendAudio(bytes) => {
                     // 只有未来有动作往 tx 里扔数据时，这里才会被唤醒并发送
-                    ws_stream
-                        .send(Message::Text(setup_msg.to_string().into()))
-                        .await
-                        .ok();
+                    ws_stream.send(Message::Binary(bytes.into())).await.ok();
                 }
                 GeminiCommand::Start { .. } | GeminiCommand::Stop | GeminiCommand::CloseSession => {
                     // Handle other command types
