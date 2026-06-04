fecd280774c9f012741df70d4253d6d45fdd7cb4
K.Chow <dogegg.chou@gmail.com>
2026-06-05 02:15:58 +0800

    feat: integrate stop recording

--- src/pages/voice/index.tsx
+++ src/pages/voice/index.tsx
@@ -14,15 +14,35 @@ const Voice = () => {
       })
   }
 
+  const handleStopRecording = async () => {
+    invoke('stop_recording')
+      .then(res => {
+        setIsRecording(false)
+        console.log(res)
+      })
+      .catch(err => {
+        console.error('Failed to stop recording:', err)
+      })
+  }
+
   return (
     <div className="max-h-full flex flex-col h-screen max-w-full">
       <div className="p-4 flex justify-center items-center h-full">
-        <button
-          className="w-36 h-36 btn btn-square btn-primary"
-          onClick={handleStartRecording}
-        >
-          {isRecording ? 'Stop voice recording' : 'Start voice recording'}
-        </button>
+        {isRecording ? (
+          <button
+            className="w-36 h-36 btn btn-square btn-warning"
+            onClick={handleStopRecording}
+          >
+            Stop voice recording
+          </button>
+        ) : (
+          <button
+            className="w-36 h-36 btn btn-square btn-primary"
+            onClick={handleStartRecording}
+          >
+            Start voice recording
+          </button>
+        )}
       </div>
     </div>
   )
